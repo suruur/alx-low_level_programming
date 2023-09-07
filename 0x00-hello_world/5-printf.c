800560bb677f8b68acfa2547ce985edaeaacb6f9
Suruur <surrarweesh@gmail.com>
2023-09-07 09:08:40 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("wiht proper grammer, but the outcome is a peice of art,\n"); 
+	return(0);
+}
