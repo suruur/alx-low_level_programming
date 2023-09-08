947d53a82112f5b22e135b29b08d5cf90ddd3595
Suruur <surrarweesh@gmail.com>
2023-09-08 16:44:40 -0700

    adding c4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter == 'e' | letter == 'q')
+		{
+			letter++;
+			continue;
+		}
+		putchar (letter);
+		letter++;
+	}
+
+	/* print a new line at the end */
+	putchar ('\n');
+	return (0);
+}
