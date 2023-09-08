b7f94e873efd93f66f9e5f7bd40108117dd6a8a1
Suruur <surrarweesh@gmail.com>
2023-09-08 16:31:18 -0700

    adding c2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar (letter);
+		letter++;
+	}
+
+	/* print a new line at the end */
+	putchar ('\n');
+	return (0);
+}
