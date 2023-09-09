f60e4cfa1c178837a66c184a60d7b87208a06e95
Suruur <surrarweesh@gmail.com>
2023-09-08 17:10:44 -0700

    adding c8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
-		putchar ('0' + i);
+	for (i = 0; i < 16; i++)
+	{	
+		if (i < 10)
+			putchar ('0' + i);
+		else 
+			/* print letters a-f as characters*/
+			putchar ('a' + i - 10);
 
 	/* print a new line at the end */
 	putchar ('\n');
