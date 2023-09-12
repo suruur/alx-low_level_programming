3ad4d6b3e74fa174bdcb9e2761d77d5610ec4dfe
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-12 18:30:55 +0200

    Update 2-print_alphabet_x10.c a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,14 @@
 #include "main.h"
 #include "1-alphabet.c"
 
-/* Function print_alphabet_x10: print alphabet in lower case 10 times */
+/**
+* print_alphabet_x10 - print alphabet in lower case 10 times 
+* Return: void
+*/
 void print_alphabet_x10(void)
 {
 	int i;
 
 	for(i = 0; i < 10; i++)
 		print_alphabet();
-
 }
