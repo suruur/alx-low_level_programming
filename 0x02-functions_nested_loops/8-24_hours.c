985b53d2b38db031edd493b8c26d0b5fc270aca5
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-13 13:40:56 +0300

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,23 +10,23 @@ void jack_bauer(void)
 {
 	int i;
 	int j;
-	int k; 
+	int k;
 	
 	for (i = 0; i < 24; i++)
 		for (j = 0; j <= 59; j++)
 		{
 			k = 0;
-			if ( i == 0 && j <= 9)
+			if (i == 0 && j <= 9)
 				printf("%d%d:%d%d\n", i, i, i, j);
 			else
-				if ( i <= 9 )
+				if (i <= 9 )
 					if (j <= 9)
-						printf("%d%d:%d%d\n", 0, i, k, j );
+						printf("%d%d:%d%d\n", 0, i, k, j);
 					else
 						printf("%d%d:%d\n", 0, i, j);
 				else
-					if ( j <=9 )
-						printf("%d:%d%d\n",  i, k, j );
+					if (j <=9 )
+						printf("%d:%d%d\n",  i, k, j);
 			k++;
 		}
 }
