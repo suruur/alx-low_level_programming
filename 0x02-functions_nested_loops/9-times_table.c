3a963e9996d4113007c92f6e1aaf2d05063d180c
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-13 18:06:38 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,8 +18,12 @@ void times_table(void)
 		for ( j = 0; j <= 9; j++)
 		{
 			t = i * j;
-        		printf("%d", t);
-			if (j != 9)
+        		/*printf("%d", t);*/
+			if (i == 0 && j == 0)
+				printf("%2d, ", t);
+			else
+				printf("%d, ", t);
+			/*if (j != 9)
 			{
 				if (t <= 9)
 				{
@@ -29,7 +33,7 @@ void times_table(void)
 				{
 					printf(", ");
 				}
-			}
+			} */
 		}
 		printf("\n");
 	}
