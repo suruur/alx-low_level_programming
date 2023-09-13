f75e9f3b05e8190eb82d289bbb7a0343c841dec3
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-13 15:50:40 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,12 +10,6 @@ int  print_last_digit(int n)
 	int r;
 	
 	r = n % 10;
-	if ( n < 0)
-	{
-		_putchar ('-');
-		_putchar (r+ '0');
-	}
-	else
-		_putchar (r+ '0');
+	_putchar (r+ '0');
 	return (r+ '0');
 }
