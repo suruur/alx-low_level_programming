803cc4d5c9f8e17467459ba2903d781f3d2739cb
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-13 18:24:53 +0200

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,20 +7,22 @@
 */
 int print_sign(int n)
 {
+	int r;
 	if (n > 0)
 	{
-		printf("+");
-		return (1);
+		_putchar('+');
+		r = 1;
 	}
 	else
 		if ( n < 0)
 		{
-			printf("-");
-			return (-1);
+			_putchar('-');
+			r = -1;
 		}
 			else
 			{
-			printf("0");
-			return (0);
+			 _putchar('0');
+			 r = 0;
 			}
+        return (r);
 }
