dae6f96ab68275032c03a9f6d2d26c52ea187d3a
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-13 12:09:07 +0300

    Update 9-times_table.c aa

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
-/*a function that prints the 9 times table,
+/**
+ * times_table - a function that prints the 9 times table,
  * starting with 0.
+ * Return: void
  */
 void times_table(void)
 {
-	if (c >= 'a' && c <= 'z')
-		r = 1;
-	else
-		r = 0;
-	
-	
-}
-	
+	int i;
+    	for (i = 0; i <= 9; i++)
+	{
+		for ( j = 0; j < = 9; i++)
+        		printf("%d ,", (i * j));
+		printf("\n");
+	}	
+}	
