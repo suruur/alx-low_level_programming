ef02606262b19411d9e3356cb3e5c15ca9792db2
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-13 17:43:38 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
  * times_table - a function that prints the 9 times table,
  * starting with 0.
  * Return: void
  */
+
 void times_table(void)
 {
 	int i;
