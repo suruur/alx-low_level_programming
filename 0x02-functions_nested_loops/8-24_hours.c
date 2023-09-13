7278343058c9c9d445c92942b37de74ff4a2d106
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-13 12:36:54 +0300

    Update 8-24_hours.c as

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * jack_bauer - a function that prints every minute of the day of Jack Bauer,
@@ -12,9 +13,5 @@ void jack_bauer(void)
 	
 	for (i = 0; i < 24; i++)
 		for (j = 0; j <= 59; j++)
-			printf( "%d%d :%d%d\n",i,i,i,j)
-				
-	
-	
+			printf( "%d%d :%d%d\n",i,i,i,j);
 }
-	
