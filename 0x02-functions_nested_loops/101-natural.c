f526eeafb070fded3ea43f1e4f6fd28113548be8
Surur Aljack <69866640+suruur@users.noreply.github.com>
2023-09-13 16:32:30 +0300

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,7 +9,7 @@ int main()
         if (i % 3 == 0 || i % 5 == 0)
             sum += i;
 
-    printf("The sum of multiples of 3 or 5 below 1024 is: %d\n", sum);
+    printf("%d\n", sum);
 
     return (0);
 }
