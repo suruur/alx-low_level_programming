c9b3c9db09b87101bbb28eac51e89613f95a05f1
Suruur <surrarweesh@gmail.com>
2023-09-14 23:30:06 +0000

    s

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,8 +9,8 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
-		_putchar(i);
+	for (i = 0; i <= 9; i++)
+		_putchar('0' + i);
 	/* print a new line at the end */
 	_putchar ('\n');
 }
