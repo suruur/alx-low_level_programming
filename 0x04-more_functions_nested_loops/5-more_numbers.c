1174550472bc3bf2a479e13b5eb37f266dac175c
Suruur <surrarweesh@gmail.com>
2023-09-14 04:50:04 -0700

    ss

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers for 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int  i = 0;
+	int j;
+
+	while(j < 10)
+	{
+		for (i = 0; i < 14; i++)
+			_putchar ('0'+ i);
+		j++;
+		_putchar ('\n');
+	}
+
+	-putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,4 +7,5 @@ int _isupper(int c);
 /* */
 void print_numbers(void);
 
-
+/* */
+void more_numbers(void);
