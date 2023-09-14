8e687f1e3a0f1b0632646c3db0e7d7ad13d05c5b
Suruur <surrarweesh@gmail.com>
2023-09-14 07:02:39 -0700

    s

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
- * print_line - Entry point
+ * print_diagonal - Entry point
  * @n: line lenght
  * Return: void
  */
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < n; i++)
+		for (j = 0; j < i; j++)
+			_putchar(' ');
 		_putchar('\\');
 
 	_putchar('\n');
