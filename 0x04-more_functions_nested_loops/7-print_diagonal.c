c21cc5574182f19924de775cc31b806d58446fbd
Suruur <surrarweesh@gmail.com>
2023-09-14 06:51:21 -0700

    s

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: line lenght
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('\\');
+
+	_putchar('\n');
+}
