cf960dafe6f679c8c5ee09b5baaf901ec5b24f4a
Suruur <surrarweesh@gmail.com>
2023-09-14 08:00:32 -0700

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,25 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
+	{
 		if ((i % 3 == 0) && (i % 5 == 0))
+		{
 			printf("BuzzFizz ");
-		else
-			if (i % 5 == 0)
-				printf("Buzz ");
-			else
-				if(i % 3 == 0)
-					printf("Fizz ");
-				else
-					printf("%d ", i);
+			continue;
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		if(i % 3 == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
 
 	printf("\n");
 	return (0);
