89544fafbc7652e7ccb7fbe6f7a9a2b742db49f1
Suruur <surrarweesh@gmail.com>
2023-09-15 00:17:47 +0000

    f

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,13 @@ void print_triangle(int size)
 	int i;
 	int j;
 	int k;
-	int limit;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
-			limit = size - i;
-			for (k = 0; k > limit; k++)
-				_putchar(' ');
+		for (k = 1; k > size - i; k++)
+			_putchar(' ');
+		for (j = 0; j <= i; j++)
 			_putchar('#');
-		}
 		_putchar('\n');
 	}
 	_putchar('\n');
--- 0x04-more_functions_nested_loops/test.c
+++ 0x04-more_functions_nested_loops/test.c
@@ -18,10 +18,9 @@ int main(void)
 	printf("%d\n", mul(98, 1024));
 	printf("%d\n", mul(-402, 4096)); */
 
-	print_square(0);
-	print_square(2);
-	print_square(10);
-	print_square(-4);
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
 
 
 	return (0);
