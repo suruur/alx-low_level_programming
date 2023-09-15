55870d5d9980131afb30a1da09a649e8d1ef77ad
Suruur <surrarweesh@gmail.com>
2023-09-15 12:24:09 +0000

    s

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+
+	}
+
+	printf("\n");
+	return (0);
+}
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,15 +7,25 @@
  */
 void print_number(int n)
 {
-	/*
-	int i;
-	
-	char *str = "" +  n;
+	if < n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-	int l = sizeof(str);
+	int rev = 0;
 
-	for (i = 0; i < l; i++) */
-		_putchar(n);
+	while (n > 0)
+	{
+		rev = rev * 10 + n % 10;
+		n /= 10;
+	}
+
+	while (n > 0)
+	{
+		_putchar(rev % 10 + '0');
+		rev /= 10;
+	}
 
 	_putchar('\n');
 }
