4b9eed47f1d96dd35ef8e697f8e79c3e527c1e60
Suruur <surrarweesh@gmail.com>
2023-09-18 09:24:25 +0000

    d

---
+++ 0x05-pointers_arrays_strings/test.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/*char c; */
+
+
+	/* c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c)); 
+	printf("%d\n", mul(98, 1024));
+	printf("%d\n", mul(-402, 4096)); */
+	/*
+	print_number(98);
+	print_number(402);
+	print_number(1024);
+	print_number(0);
+	print_number(-98); 
+
+
+	more_numbers();*/
+	int n;
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+
+	return (0);
+}
