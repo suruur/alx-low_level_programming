74cc13f77f4c927544cb0183f6c4f8632a0f2eb3
Suruur <surrarweesh@gmail.com>
2023-09-18 12:37:43 +0000

    d

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * rev_string - prints string..
+ * @s: string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char tmp;
+
+	int len = strlen(s);
+
+	for (i = 0, j = len - 1; i > j; i++, j--)
+	{
+		tmp = s[i];
+		s[i] = s[j]
+		s[j] = tmp;
+	}
+}
--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -35,10 +35,14 @@ int main(void)
 	printf("a=%d, b=%d\n", a, b);
 	swap_int(&a, &b);
 	printf("a=%d, b=%d\n", a, b); */
-	char *str;
+	char s[10] = "My School";
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
+	/*str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);*/
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+
 
 	return (0);
 }
