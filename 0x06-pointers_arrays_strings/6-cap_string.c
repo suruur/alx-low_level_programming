0f6e9a47cea5fa21c30a34a872b811e09c6be2f2
Suruur <surrarweesh@gmail.com>
2023-09-21 10:14:11 +0000

    k

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -25,8 +25,9 @@ char *cap_string(char *a)
 				|| a[i] == '(' || a[i] == '{'
 				|| a[i] == '}' || a[i] == '\n'
 				|| a[i] == '\t')
-			if (i + 1 != len)
-				a[i + 1] = toupper((unsigned char)a[i + 1]);
+			if (i + 1 != len &&)
+				if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
+					a[i + 1] = toupper((unsigned char)a[i + 1]);
 
 	return (a);
 }
