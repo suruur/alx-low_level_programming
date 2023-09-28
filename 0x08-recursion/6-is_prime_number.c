9dde59d305f6f3af3c584d4d2aca6d19db51efe5
Suruur <surrarweesh@gmail.com>
2023-09-28 11:56:35 +0000

    fff

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,19 +1,34 @@
 #include "main.h"
-
 /**
- * _memset - concat string.
- * @s: string pointer
- * @b: filling char
- * @n: int length
- * Return:  pointer
+ * is_primee - tmp
+ * @n: int
+ * @d: int
+ * Return: int
  */
-char *_memset(char *s, char b, unsigned int n)
+
+int is_primee(int n, int d)
 {
-	unsigned int i;
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+		if (d == 1)
+			return (1);
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
+	if (n % d == 0)
+		return (0);
 
+	return (is_primee(n, n - 1));
 
-	return (s);
+}
+/**
+ * is_prime_number - checks if n is prime or not
+ * @n: int length
+ * Return:  int
+ */
+
+int is_prime_number(int n)
+{
+	return (is_primee(n, n - 1));
 }
