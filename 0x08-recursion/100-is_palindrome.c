f738d2062ed07bcb58dd3fe2c4d8580ba9eed058
Suruur <surrarweesh@gmail.com>
2023-09-28 14:35:08 +0000

    ff

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,24 @@
 #include "main.h"
 #include <string.h>
 /**
+ * tmp - tmp duc
+ * @s: string
+ * @st: int
+ * @e: end
+ * Return: int
+ */
+int tmp(char *s, int st, int e)
+{
+	if (st >= e)
+		return (1);
+
+
+	if (s[st] == s[e])
+		return (tmp(s, st + 1, e - 1));
+
+	return (0);
+}
+/**
  * is_palindrome - prints string.
  * @s: string pointer
  * Return: int
@@ -9,22 +27,7 @@ int is_palindrome(char *s)
 {
 	int len;
 
+	len = strlen(s);
 
-	if (strlen(s) <= 1)
-	{
-		return (1);
-	}
-	else
-		len = strlen(s);
-
-	if (s[0] != s[len - 1])
-	{
-		return (0);
-	}
-	else
-	{
-		s[len - 1] = '\0';
-		return (is_palindrome(s + 1));
-	}
-
+	return (tmp(s, 0, len - 1));
 }
