0f3ade29ca825222d3a03906dc2fd19517a1bf1c
Suruur <surrarweesh@gmail.com>
2023-10-02 09:59:54 +0000

    d

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: args size
+ * @argv: args list
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
+}
