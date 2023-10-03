3cab51d09c4916ed9c8cbc375449a439a894b47e
Suruur <surrarweesh@gmail.com>
2023-10-03 13:09:15 +0000

    g

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,9 +26,9 @@ int **alloc_grid(int width, int height)
 
 		if (grid[i] == NULL)
 		{
-			for (j = 0; j < i; j++)
+			/*for (j = 0; j < i; j++)
 				free(grid[j]);
-			free(grid);
+			free(grid);*/
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
