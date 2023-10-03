269866ec4b2f5c128db012d5584d3e9ee504c9b0
Suruur <surrarweesh@gmail.com>
2023-10-03 10:11:23 +0000

    f

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - creates array
+ * @str: array size
+ * Return: array
+ */
+char *_strdup(char *str)
+{
+	char *s;
+
+	s = malloc(sizeof(str));
+	s = str;
+
+	if (!s)
+		return (NULL);
+
+
+	return (s);
+}
