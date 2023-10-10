9fc67d6125c14a341dcc95c4cb113acea4c53550
Suruur <surrarweesh@gmail.com>
2023-10-10 12:59:20 +0000

    h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,18 +6,16 @@
  * @age: age
  * @owner: owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
-
-#typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 
