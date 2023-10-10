6c8aac07d84d93a82f9de8fbe4dd3fbea9067c46
Suruur <surrarweesh@gmail.com>
2023-10-10 13:19:32 +0000

    g

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,8 +20,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	d->age = age;
 	strcpy(d->name, name);
+	d->age = age;
 	strcpy(d->owner, owner);
 
 	return (d);
