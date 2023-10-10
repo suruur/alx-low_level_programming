991601590135f7ee80e4e4f92f29c7a950e7d367
Suruur <surrarweesh@gmail.com>
2023-10-10 11:24:29 +0000

    f

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,5 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - Entry point
  *
@@ -8,34 +7,7 @@
  */
 int main(void)
 {
-	/*char c; */
-
-
-	/* c = '0';
-	printf("%c: %d\n", c, _isdigit(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isdigit(c)); 
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096)); */
-	/*
-	print_number(98);
-	print_number(402);
-	print_number(1024);
-	print_number(0);
-	print_number(-98); */
-
-
-	char *s;
-
-	s = _strdup("ALX SE");
-
-	if (s == NULL)
-	{
-		printf("failed alocation \n");
-		return (1);
-	}
-	printf("%s\n", s);
-	 free(s);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
