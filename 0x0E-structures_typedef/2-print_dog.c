39a32771adf3e793f342df083a16e5ddbaf85da9
Suruur <surrarweesh@gmail.com>
2023-10-10 13:34:18 +0000

    g

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d->name == NULL || d->age == 0 || d->owner == NULL)
+	if (d->name == NULL || d->age == 0.0 || d->owner == NULL)
 	{
 		printf("(nil)\n");
 	}
