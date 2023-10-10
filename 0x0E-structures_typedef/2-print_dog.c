512157c28bfc2e9ed5be498b48a0940022e10c59
Suruur <surrarweesh@gmail.com>
2023-10-10 13:21:28 +0000

    g

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,5 +14,5 @@ void print_dog(struct dog *d)
 		printf("(nil)\n");
 	}
 
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %6f\nOwner: %s\n", d->name, d->age, d->owner);
 }
