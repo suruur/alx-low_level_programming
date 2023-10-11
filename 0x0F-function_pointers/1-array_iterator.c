aa3bdd35845eec74d80f4a87fefb1a163a303d2b
Suruur <surrarweesh@gmail.com>
2023-10-11 09:23:48 +0000

    g

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print name
+ * @array: name
+ * @size: p to function
+ * @action: action
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
 void print_name(char *name, void (*f)(char *));
 
+void array_itrerator(int *array, size_t size, void (*action)(int));
+
 
