62e9fcb062bc0cda2e61bb74db62f43618bce1d0
Suruur <surrarweesh@gmail.com>
2023-10-11 09:32:14 +0000

    g

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
 #include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 
 void array_itrerator(int *array, size_t size, void (*action)(int));
 
-
+#endif
