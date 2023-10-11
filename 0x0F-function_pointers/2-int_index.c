27c1d1ab66e4d9b6a3e52017259a3c873adc68dd
Suruur <surrarweesh@gmail.com>
2023-10-11 12:09:32 +0000

    g

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,10 +9,12 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
 	if (size  <= 0)
 		return (-1);
-	int i;
-	for ( i = 0; i < size; i++)
+
+	for (i = 0; i < size; i++)
 		if (cmp(array[i]) != 0)
 			return (i);
 
