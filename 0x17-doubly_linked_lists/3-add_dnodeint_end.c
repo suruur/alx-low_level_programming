ecaa08d07879258ab6512e3ec999b98fa7116303
suruur <surrarweesh@gmail.com>
2023-12-07 11:02:16 +0000

    3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - print douly linked list
+ * @head: pointer to the d list
+ * @n: count
+ * Return: number of nodes
+ */
+dlistint_t *add_nodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *current;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
+	else
+	{
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		new_node->prev = current;
+		current->next = new_node;
+	}
+
+	return (new_node);
+}
